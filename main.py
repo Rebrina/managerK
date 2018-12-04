890859286cbc42d2fe275608193f42240bca661e
Rebrina <feurioster@gmail.com>
2018-12-05 00:52:22 +0300

    .

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import time
 import subprocess
-start_time = time.time()
 
-class Timer:
+class timer:
     def __init__(self, name):
         self.name = name
 
     def __enter__(self):
         self.start = time.time()
-        print("--- %s seconds ---" % (self.start))
+        print("Program run start: %0.3f seconds" % (self.start))
 
     def __exit__(self, *args):
         self.end = time.time()
         self.interval = self.end - self.start
+        print("Program run end: %0.3f seconds" % (self.end))
         print("%s took: %0.3f seconds" % (self.name, self.interval))
         return False
 
-with Timer('Вызов браузера'):
+with timer('Browser call'):
      subprocess.Popen([r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"])
-
-print("--- %s seconds ---" % (time.time() - start_time))
